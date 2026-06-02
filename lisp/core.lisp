531e48c79b1b16247188434fae7d9967b6be48d3
Fran25100 <kuritokurokurote@gmail.com>
2026-06-02 20:44:14 -0300

    Update core.lisp

--- lisp/core.lisp
+++ lisp/core.lisp
@@ -1,9 +1,8 @@
 ( defun transicion (color-actual cambiar-a)
     (cond
-      ((and (eq color-actual "rojo" ) (eq cambiar-a "verde")) (list color-actual "cambiar-a-verde"));nuestra primera condicion, comparamos variables(color-actual y cambiar-a)
-                                                                                                    ;y si cumple creamos una lista con el color actual y el color a cambiar
-      ((and (eq color-actual "verde" ) (eq cambiar-a "amarillo")) (list color-actual "cambiar-a-amarillo"))
-      ((and (eq color-actual "amarillo" ) (eq cambiar-a "rojo")) (list color-actual "cambiar-a-rojo"))
-      (t (list color-actual "color-por-defecto"))
+      ((and (eq color-actual 'rojo ) (eq cambiar-a 'verde)) (list color-actual "cambiar-a-verde"))
+      ((and (eq color-actual 'verde ) (eq cambiar-a 'amarillo)) (list color-actual "cambiar-a-amarillo"))
+      ((and (eq color-actual 'amarillo ) (eq cambiar-a 'rojo)) (list color-actual "cambiar-a-rojo"))
+      (t (list color-actual 'color-por-defecto))
     )
 )
