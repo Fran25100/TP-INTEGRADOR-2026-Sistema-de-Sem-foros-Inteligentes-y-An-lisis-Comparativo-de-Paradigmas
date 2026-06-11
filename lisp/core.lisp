e988455481208b2b8220a36c7f82b6bb82201032
Fran25100 <kuritokurokurote@gmail.com>
2026-06-11 19:19:02 -0300

    Update core.lisp

--- lisp/core.lisp
+++ lisp/core.lisp
@@ -43,148 +43,27 @@ break 1 [3]> (transicion 'EN-AMARILLO 'ACCION-POR-DEFECTO)
 
 (EN-AMARILLO ACCION-POR-DEFECTO)
 
-Break 1 [3]> ;-------------------------------------------------------------------------------------------------------------------
+Break 3 [5]> ;-------------------------------------------------------------------------------------------------------------------
 #| Funcion: timer
 				Naturaleza: Impura (escribe en pantalla segun los datos de entrada)
-				Estrategia: Orden Superior (implementada con cond, and y mod)
+				Estrategia: Orden Superior (implementada con format)
 				Impacto En Memoria: No Destructiva, no realiza cambios
-
  |#
 ;-------------------------------------------------------------------------------------------------------------------
 
-(defun time (unix)
-				(cond (and (>= (mod unix 216) 0) (<= (mod unix 216) 89) "rojo")
-				((and (>= (mod unix 216) 90) (<= (mod unix 216) 95)) "amarillo")
-				((and (>= (mod unix 216) 96) (<= (mod unix 216) 216)) "verde")
-)
-)
-
-** - Continuable Error
-DEFUN/DEFMACRO(TIME): #<PACKAGE COMMON-LISP> is locked
-Si contin■a (tecleando `continue'): Ignore the lock and proceed
-The following restarts are also available:
-ABORT          :R1      Abort debug loop
-Break 2 [4]> (defun time-unix (unix)
-				(cond (and (>= (mod unix 216) 0) (<= (mod unix 216) 89) "rojo")
-				((and (>= (mod unix 216) 90) (<= (mod unix 216) 95)) "amarillo")
-				((and (>= (mod unix 216) 96) (<= (mod unix 216) 216)) "verde")
-)
+( defun timer (timestap )
+    (let ((segundos (mod timestap 225)))
+        (cond
+            ((and (>= segundos 0) (<= segundos 89)) 'en-rojo )
+            ((and (>= segundos 90) (<= segundos 92)) 'en-rojo-intermitente )
+            ((and (>= segundos 93) (<= segundos 212)) 'en-verde )
+            ((and (>= segundos 213) (<= segundos 215)) 'en-verde-intermitente )
+            ((and (>= segundos 216) (<= segundos 221)) 'en-amarillo )
+            ((and (>= segundos 222) (<= segundos 224)) 'en-amarillo-intermitente ) 
+        )
+    )
 )
 
-TIME-UNIX
-Break 2 [4]> (time-unix 216)
-
-*** - COND: variable AND has no value
-Es posible continuar en los siguientes puntos:
-USE-VALUE      :R1      Input a value to be used instead of AND.
-STORE-VALUE    :R2      Input a new value for AND.
-ABORT          :R3      Abort debug loop
-ABORT          :R4      Abort debug loop
-Break 3 [5]> (defun time-unix (unix)
-				(cond ((and (>= (mod unix 216) 0) (<= (mod unix 216) 89)) "rojo")
-				((and (>= (mod unix 216) 90) (<= (mod unix 216) 95)) "amarillo")
-				((and (>= (mod unix 216) 96) (<= (mod unix 216) 216)) "verde")
-)
-)
-
-
-TIME-UNIX
-Break 3 [5]> (time-unix 216)
-
-"rojo"
-Break 3 [5]> (time-unix 509)
-
-"rojo"
-Break 3 [5]> (time-unix 1024)
-
-"verde"
-Break 3 [5]> (time-unix 894)
-
-"rojo"
-Break 3 [5]> (time-unix 1000)
-
-"verde"
-Break 3 [5]> (time-unix 985)
-
-"verde"
-Break 3 [5]> (time-unix 935)
-
-"rojo"
-Break 3 [5]> (time-unix 945)
-
-"rojo"
-Break 3 [5]> (time-unix 955)
-
-"amarillo"
-Break 3 [5]> (time-unix 951)
-
-"rojo"
-Break 3 [5]> (time-unix 952)
-
-"rojo"
-Break 3 [5]> (time-unix 953)
-
-"rojo"
-Break 3 [5]> (time-unix 954)
-
-"amarillo"
-Break 3 [5]> (time-unix 864)
-
-"rojo"
-Break 3 [5]> (time-unix 863)
-
-"verde"
-Break 3 [5]> #| como se puede observar, el limite inferior de rojo en este ejemplo es de 953, por lo que al restar 90s, se obtiene
-el valor de "verde", pero si en vez de eso se restara un valor menor a 90s, se obtiene el valor de amarillo. esto debido
-al ciclo del semaforo: verde->amarillo->rojo->verde...|#
-(print "actualizacion")
-
-"actualizacion" 
-"actualizacion"
-Break 3 [5]> (time-unix 1034)
-
-"verde"
-Break 3 [5]> (time-unix 1033)
-
-"verde"
-Break 3 [5]> (time-unix 954)
-
-"amarillo"
-Break 3 [5]> (time-unix 953)
-
-"rojo"
-Break 3 [5]> (time-unix 953)
-
-"rojo"
-Break 3 [5]> #|1-CORRECCION:como se puede observar, el limite SUPERIOR de rojo en este ejemplo es de 953, por lo que al restar 90s, se obtiene
-el valor de "verde", pero si en vez de eso se sumara un valor de hasta maximo a 6s, se obtiene el valor de amarillo. esto debido
-al ciclo del semaforo: verde->amarillo->rojo->verde... al restar 90 nos estariamos ubicando en forma de ciclo contrario
-						verde->rojo->amarillo->verde|#
-(print "NUEVA ACTUALIZACION")
-
-"NUEVA ACTUALIZACION" 
-"NUEVA ACTUALIZACION"
-;-------------------------------------------------------------------------------------------------------------------
-#| Funcion: timer
-				Naturaleza: Impura (escribe en pantalla segun los datos de entrada)
-				Estrategia: estructura condicional (implementada con cond)
-				Impacto En Memoria: No Destructiva, no realiza cambios
-
- |#
-;-------------------------------------------------------------------------------------------------------------------
-Break 3 [5]> #|(defun timer (timeUnix)
-				(cond ((and (>= (mod unix 216) 0) (<= (mod unix 216) 89)) "rojo")
-				((and (>= (mod unix 216) 96) (<= (mod unix 216) 216)) "verde")
-)
-)|#
-(defun timer (timeUnix)
-				(cond ((and (>= (mod timeUnix 216) 0) (<= (mod timeUnix 216) 89)) "rojo")
-				((and (>= (mod timeUnix 216) 90) (<= (mod timeUnix 216) 95)) "amarillo")
-				(t "verde")
-)
-)
-
-TIMER
 Break 3 [5]> ;-------------------------------------------------------------------------------------------------------------------
 #| Funcion: LogginLights
 				Naturaleza: Impura (escribe en pantalla segun los datos de entrada)
