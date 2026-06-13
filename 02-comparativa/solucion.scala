da6e5b2655e6855d3e1b551af9ba6710566f09f3
Fran25100 <kuritokurokurote@gmail.com>
2026-06-13 12:39:26 -0300

    Update solucion.scala

--- 02-comparativa/solucion.scala
+++ 02-comparativa/solucion.scala
@@ -1,1 +1,8 @@
+import scala.compiletime.ops.string //para poder utilizar las variabales string
 
+/* funcion: transicion */
+def transicion(colorActual: string.type, colorACambiar: string.type) = { // declare la funcion, como parametro las variables string
+  colorActual match { // funciona como un cond
+    case "rojo" => "verde"
+  }
+}
