6a0495a14164ca5f9716e1bb74652c04ffeb371a
Fran25100 <kuritokurokurote@gmail.com>
2026-06-13 13:57:20 -0300

    Update solucion.scala

--- 02-comparativa/solucion.scala
+++ 02-comparativa/solucion.scala
@@ -1,10 +1,10 @@
-//decidi sacar la libreria string porque se puede declarar String sin necesidad de la libreria
 /* funcion: transicion */
-def transicion(colorActual: String) = { // declare la funcio y como parametro la variable string
-  var colorACambiar =
-  colorActual match { // funciona como un cond
-    case "rojo" => "verde" 
-    case "verde" => "amarillo"
-    case "amarillo" => "rojo"
+def transicion(colorActual: String, colorACambiar: String) = { // declare la funcion y como parametros las variables strings
+  val colores = List(colorActual, colorACambiar)
+  colorActual match { // funciona como un cond, por ahora es simple luego agrego las intermitentes
+    case "rojo"     => (colorACambiar == "verde") // caso1
+    case "verde"    => (colorACambiar == "amarillo") // caso2, no creo que este termina o bien todavia
+    case "amarillo" => (colorACambiar == "rojo") // caso3
   }
+  println(colores) // imprimiria en pantalla la lista
 }
